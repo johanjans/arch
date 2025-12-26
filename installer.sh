86d6c73f0d74c84af1122e9829334e989a017b7e
Johan Jansson <johan.jansson@ju.se>
2025-12-26 20:56:46 +0100

    removed swedish locale

--- installer.sh
+++ installer.sh
@@ -81,7 +81,6 @@ genfstab -pU /mnt >> /mnt/etc/fstab
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 arch-chroot /mnt hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-echo "sv_SE.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 echo "KEYMAP=sv-latin1" > /mnt/etc/vconsole.conf
